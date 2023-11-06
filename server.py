f0b3bdaf16d0f5495c9692794020b93b2c2e844b
Can Günyel <cangunyel@gmail.com>
2023-11-07 01:33:51 +0300

    suppliers.html created and server.py updated

--- server.py
+++ server.py
@@ -1,10 +1,31 @@
 import mysql.connector
+from flask import Flask, render_template
 
 db = mysql.connector.connect(
-        host="northwind.cfl4fp0ymkxx.eu-north-1.rds.amazonaws.com",
-        user="admin",
-        password="dh8zcJMSGQhlcHT554yX",
-        database="northwind"
+    host="northwind.cfl4fp0ymkxx.eu-north-1.rds.amazonaws.com",
+    user="admin",
+    password="dh8zcJMSGQhlcHT554yX",
+    database="northwind"
 )
 
-cursor = db.cursor()
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+     return render_template('base.html',)
+
+@app.route('/suppliers')
+def supplier():
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM suppliers;")
+    records = cursor.fetchall()
+
+    cursor.close()
+
+    
+    db.close()
+
+    return render_template('suppliers.html', records=records)
+
+if __name__ == '__main__':
+    app.run(debug=True)
