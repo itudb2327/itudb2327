baafb04c5f95780d96876e1f34e1463a49f9c738
Can Günyel <cangunyel@gmail.com>
2023-12-20 03:39:19 +0300

    missing file added.

--- purchases.py
+++ purchases.py
@@ -1,6 +1,6 @@
 import mysql.connector
-from flask import Flask, render_template, request,redirect,session
-
+from flask import Flask, render_template, request,redirect,session,redirect
+import re
 
 class Purchase:
     def __init__(self, id=None, supplier_id=None, created_by=None, submitted_date=None,
@@ -24,7 +24,30 @@ class Purchase:
         self.approved_date = approved_date
         self.submitted_by = submitted_by
 
-from flask import redirect
+
+def return_record(cursor,active_filter):
+    cursor.execute("""SELECT    purchase.id,
+                                suppliers.company,
+                                employees.last_name,
+                                purchase.submitted_date,
+                                purchase.creation_date,
+                                purchase.status_id,
+                                purchase.expected_date,
+                                purchase.shipping_fee,
+                                purchase.taxes,
+                                purchase.payment_date,
+                                purchase.payment_method,
+                                purchase.notes,
+                                purchase.approved_by,
+                                purchase.approved_date,
+                                purchase.submitted_by
+                                FROM purchase_orders as purchase 
+                                INNER JOIN suppliers ON purchase.supplier_id=suppliers.id 
+                                INNER JOIN employees ON purchase.created_by=employees.id 
+                                WHERE status_id=%s;""",(active_filter,))
+    records = cursor.fetchall()  
+    return records
+
 
 def create_record(cursor, temp_purchase):
     statement = """
@@ -68,6 +91,47 @@ def create_record(cursor, temp_purchase):
     cursor.execute(statement, values)
 
     return redirect("purchases")
+
+def get_all_purchases(db):
+    cursor = db.cursor()
+    purchase_list = []
+    active_filter = session.get('active_filter')
+    select_query = """SELECT id, supplier_id, created_by, submitted_date, 
+                             creation_date, status_id, expected_date, shipping_fee, 
+                             taxes, payment_date, payment_method, notes, 
+                             approved_by, approved_date, submitted_by 
+                      FROM purchase_orders WHERE status_id=%s"""
+    cursor.execute(select_query,(active_filter,))
+    results=cursor.fetchall()
+    # print(results)
+    for (
+        purchase_id, supplier_id, created_by, submitted_date,
+        creation_date, status_id, expected_date, shipping_fee,
+        taxes, payment_date, payment_method, notes,
+        approved_by, approved_date, submitted_by
+    ) in results:
+        purchase_list.append(
+            Purchase(
+                id=purchase_id,
+                supplier_id=supplier_id,
+                created_by=created_by,
+                submitted_date=submitted_date,
+                creation_date=creation_date,
+                status_id=status_id,
+                expected_date=expected_date,
+                shipping_fee=shipping_fee,
+                taxes=taxes,
+                payment_date=payment_date,
+                payment_method=payment_method,
+                notes=notes,
+                approved_by=approved_by,
+                approved_date=approved_date,
+                submitted_by=submitted_by
+            )
+        )
+
+    cursor.close()
+    return purchase_list,active_filter
 
 
 def update_record(cursor, updateId, temp_purchase):
@@ -123,28 +187,20 @@ def delete_record(cursor,updateId):
 def index(db):
     
     cursor = db.cursor()
-    active_filter="-1"
+    active_filter=-1
     records=[]
+    # print(request.form.keys())
     if request.method =='POST':
-        print(request.form.keys())
+        if 'querry' in request.form:
+            search_for=request.form['search_for']
+            querry =request.form['querry']
+            records,active_filter=search_record(db,search_for,querry)
         if 'filter' in request.form:
-            filter=request.form["filter"]
-            if filter == "0":#add new record
-                active_filter="0"
-                cursor.execute("SELECT * FROM purchase_orders WHERE status_id=0;")
-                records = cursor.fetchall()    
-            elif filter == "1":#add new record
-                active_filter="1"
-                cursor.execute("SELECT * FROM purchase_orders WHERE status_id=1;")
-                records = cursor.fetchall()    
-            elif filter == "2":#add new record
-                active_filter="2"
-                cursor.execute("SELECT * FROM purchase_orders WHERE status_id=2;")
-                records = cursor.fetchall()    
-            elif filter == "3":#add new record
-                active_filter="3"
-                cursor.execute("SELECT * FROM purchase_orders WHERE status_id=3;")
-                records = cursor.fetchall()
+            if 'filter' in request.form:
+                filter = request.form["filter"]
+                # Set the active_filter in the session
+                session['active_filter'] = filter
+            records = return_record(cursor,filter)  
         if 'operation' in request.form:
             operation=request.form["operation"]
             if operation == "0":  # add new record
@@ -194,9 +250,51 @@ def index(db):
             formatted_time = now.strftime('%Y-%m-%d %H:%M:%S')
             cursor.execute("UPDATE TableLastUpdateInfo SET update_time=%s WHERE table_name='purchase_orders';",(formatted_time,))
             db.commit()#commit for all filters        
-    cursor.execute("SELECT update_time FROM TableLastUpdateInfo where table_name='purchase_orders';")
-    update_time=cursor.fetchall()
-    update_time=(update_time[0][0]).strftime("%Y-%m-%d %H:%M:%S")#reaching timestamp information and adjust it to UTC+03:00 timezone
-    cursor.close()
-
+    update_time=display_last_update(cursor)
+    
     return render_template('purchases.html',records=records,update_time=update_time,active_filter=active_filter)
+
+def search_record(db,search_for,querry):
+    
+    purchase_list,active_filter = get_all_purchases(db)
+    records=[]
+    filtered_products=[]
+    querry = querry.lower()
+    filtered_list = []
+    for purchase in purchase_list:
+            
+        searched_item = getattr(purchase, search_for, None)
+        if re.findall(querry,str(searched_item)) :
+            filtered_products.append(purchase)      
+    for temp_purchase in filtered_products:
+        
+        record=(temp_purchase.id,
+                temp_purchase.supplier_id,
+                temp_purchase.created_by,
+                temp_purchase.submitted_date,
+                temp_purchase.creation_date,
+                temp_purchase.status_id,
+                temp_purchase.expected_date,
+                temp_purchase.shipping_fee,
+                temp_purchase.taxes,
+                temp_purchase.payment_date,
+                temp_purchase.payment_method,
+                temp_purchase.notes,
+                temp_purchase.approved_by,
+                temp_purchase.approved_date,
+                temp_purchase.submitted_by)
+        records.append(record)
+    
+    for record in records:
+        print(record[0],record[1])
+    
+    
+    return records,active_filter
+
+
+def display_last_update(cursor):
+        cursor.execute("SELECT update_time FROM TableLastUpdateInfo where table_name='purchase_orders';")
+        update_time=cursor.fetchall()
+        update_time=(update_time[0][0]).strftime("%Y-%m-%d %H:%M:%S")#reaching timestamp information and adjust it to UTC+03:00 timezone
+        cursor.close()
+        return update_time
